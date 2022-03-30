53114e9981b5319e8ac2fea5b178787d07cc4a87
moosman <moosman@LPRUHE0746.ejada.com>
2022-03-30 15:58:59 +0800

    adding profiles and logs

--- src/main/java/com/example/demo/controller/PostalcodeController.java
+++ src/main/java/com/example/demo/controller/PostalcodeController.java
@@ -5,6 +5,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,28 +27,35 @@ import com.example.demo.service.PostalcodeService;
 @RequestMapping("/api/postalcode")
 public class PostalcodeController {
 	
+	private Logger logger = LoggerFactory.getLogger(PostalcodeController.class);
+	
 	@Autowired
     PostalcodeService postalcodeService;
 	
 	@GetMapping("/getAll")
 	public List<Postalcode> getAll() {
+		logger.info("Call of getAll() API");
 		return postalcodeService.getAll();
 	}
 	
 	@PostMapping("/save")
 	public boolean savePostalcode(@RequestBody Postalcode postalcode) {
-		System.out.println("Postalcode : "+postalcode);
+		logger.info("Call of savePostalcode API");
+		logger.info("Postalcode : "+postalcode);
 		return postalcodeService.save(postalcode);
 	}
 	
 	@PostMapping("/bulksave")
 	public int savePostalcode(@RequestBody List<Postalcode> postalcodes) {
+		logger.info("Call of bulksave API");
+		logger.info("inserted records :: "+ postalcodes.size());
 		return postalcodeService.saveBulk(postalcodes);
 	}
 	
 	@GetMapping("/getSuburbnames")
 	public List<String> getSuburbNames(@RequestBody Range range) {
-		System.out.println("range = "+ range);
+		logger.info("Call of getSuburbnames API");
+		logger.info("range = "+ range);
 	 return postalcodeService.getSuburbnames(range);
 	}
 
